a738d6b1a72be4d74f91b24cd15a3fdec7514dec
RizkyPM40076 <rizky.213040097@mail.unpas.ac.id>
2023-10-31 23:07:43 +0700

    Video3 Struktur Folder,Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rizky Priya",
+        "email" => "rizky.213040097@mail.unpas.ac.id",
+        "image" => "Me.jpg"
+    ]);
+});
+
+
+
+Route::get('/blog', function () {
+    return view ('posts');
 });
