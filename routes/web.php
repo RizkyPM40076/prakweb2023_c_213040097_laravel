ce150f9c1e7d0795600756b86b41a22d619e0368
RizkyPM40076 <rizky.213040097@mail.unpas.ac.id>
2023-12-05 17:28:02 +0700

    video 16

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,13 @@ Route::get('/categories', function(){
 });
 
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
